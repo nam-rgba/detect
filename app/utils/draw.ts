1e34029ab6335baafccfdaa355ba2bd13ba3c073
Nam <doanngocnam3002@gmail.com>
2024-03-27 16:27:49 +0700

    70%

---
+++ app/utils/draw.ts
@@ -0,0 +1,11 @@
+import { DetectedObject, ObjectDetection } from "@tensorflow-models/coco-ssd"
+
+// mirrored, prediction,canvasRef.current?.getContext('2d')
+export function drawOnCanvas(    mirrored:boolean,
+    prediction: DetectedObject,
+    ctx: CanvasRenderingContext2D | null | undefined
+    ){
+
+        prediction
+
+}
